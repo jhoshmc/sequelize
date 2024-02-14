b126bc6af48c5083e95850a36ed6b1ece4e8512a
caleb <jhoshmc201@gmail.com>
2024-02-14 10:03:06 -0500

    creando modelo Task

--- prueba_1/src/db.js
+++ prueba_1/src/db.js
@@ -2,13 +2,15 @@ require("dotenv").config;
 const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const proyectModel = require("./models/proyect");
+const taskModel = require("./models/task");
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/${DB_NAME}`,
   { logging: false }
 );
 proyectModel(sequelize);
+taskModel(sequelize);
 
-const { Proyect } = sequelize.models;
+const { Proyect, Task } = sequelize.models;
 
 module.exports = { ...sequelize.models, sequelize };
---
+++ prueba_1/src/models/task.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) =>
+  sequelize.define(
+    "Task",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      done: {
+        type: DataTypes.BOOLEAN,
+        defaulValue: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
