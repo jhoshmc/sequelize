d43e4d2464b24d266e9cf16a7cf142d8f8f50f9b
caleb <jhoshmc201@gmail.com>
2024-02-17 10:09:52 -0500

    trayendo usuario con sus post

--- PostsUser/src/handlers/user.handler.js
+++ PostsUser/src/handlers/user.handler.js
@@ -1,10 +1,10 @@
-const { User } = require("../db");
+const { User, Post } = require("../db");
 
 const getUsers = async (req, res) => {
   try {
     const { id } = req.params;
     if (id) {
-      const post = await User.findByPk(id);
+      const post = await User.findByPk(id, { include: { model: Post } });
       if (!post) return res.status(400).send("usuario no encontrado");
       return res.status(200).json(post);
     }
