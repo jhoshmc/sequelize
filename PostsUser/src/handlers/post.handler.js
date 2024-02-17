3b458733e75092f6a947c66ecc34ea5e6a846bcd
caleb <jhoshmc201@gmail.com>
2024-02-17 09:59:23 -0500

    asociacion al crear un post con un usuario

--- PostsUser/src/handlers/post.handler.js
+++ PostsUser/src/handlers/post.handler.js
@@ -15,10 +15,11 @@ const getPosts = async (req, res) => {
 };
 const createPosts = async (req, res) => {
   try {
-    const { title, body } = req.body;
+    const { userId, title, body } = req.body;
     if (!title || !body) return res.status(400).send("faltan completar datos ");
     const newPost = await Post.create({ title, body });
-    return res.status(200).json(newPost);
+    const post = await newPost.setUser(userId);
+    return res.status(200).json(post);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
