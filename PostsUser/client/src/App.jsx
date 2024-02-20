3808e634a0afe04472e7ab090af64a0f12aa8d84
caleb <jhoshmc201@gmail.com>
2024-02-19 21:00:47 -0500

    front

---
+++ PostsUser/client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import Landing from "./views/Landing";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
