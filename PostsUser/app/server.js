0a449b9132b318351464ab0d67a63f787789e141
caleb <jhoshmc201@gmail.com>
2024-02-20 20:21:45 -0500

    agregando cors

--- PostsUser/app/server.js
+++ PostsUser/app/server.js
@@ -4,6 +4,17 @@ const userRoutes = require("./src/routes/user.routes");
 const postRouter = require("./src/routes/post.routes");
 //midelwares
 server.use(express.json());
+// cors
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 server.use(userRoutes);
 server.use(postRouter);
 
