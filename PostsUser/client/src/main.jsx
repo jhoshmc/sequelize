3c69a429c567ef326e1e2089092d875122cd0d68
caleb <jhoshmc201@gmail.com>
2024-02-25 00:02:14 -0500

    poniendo endpoind general

--- PostsUser/client/src/main.jsx
+++ PostsUser/client/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import "./index.css";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
