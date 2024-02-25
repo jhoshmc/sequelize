aed57c4e38066502912bd170454a17ce2db33234
caleb <jhoshmc201@gmail.com>
2024-02-25 00:06:42 -0500

    instalando extencion de redux-devtools

--- PostsUser/client/src/redux/store.js
+++ PostsUser/client/src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 // thunk es un midelware, una funcion intermediaria , que se ejecuta cuando se hace una peticion
 
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
